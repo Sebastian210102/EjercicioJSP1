774b0c49e3c2f6caec5af13c6843043b46d7f4a7
Sebastian210102 <72526765+Sebastian210102@users.noreply.github.com>
2024-02-08 23:05:19 -0600

    Curso JSP1

---
+++ app.js
@@ -0,0 +1,9 @@
+//Ejercicio 1
+alert("Esta es nuestra pagina web");
+
+//Ejercicio 2
+let numeroSuerte = 25;
+//Ejercico3
+let nombreUsuario = prompt("Danos tu nombre");
+//Ejercicio 4
+alert("Hola " + nombreUsuario + " tu numero de la suerte es: " + numeroSuerte );
